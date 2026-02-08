df7ad4717cc916112219eca0e4fcaa5cbe06771a
FadelSM <rasyahstore8888@gmail.com>
2026-02-08 13:38:19 +0700

    Update HotelPrices.java

--- HotelPrices.java
+++ HotelPrices.java
@@ -1,5 +1,3 @@
-
-
 #include <iostream>
 #include <vector>
 
@@ -8,12 +6,15 @@ using namespace std;
 class HotelRoom {
 public:
     HotelRoom(int bedrooms, int bathrooms) 
-    : bedrooms_(bedrooms), bathrooms_(bathrooms) {}
-    
-    int get_price() {
-        return 50*bedrooms_ + 100*bathrooms_;
+        : bedrooms_(bedrooms), bathrooms_(bathrooms) {}
+
+    virtual int get_price() {   // FIX 1: virtual
+        return 50 * bedrooms_ + 100 * bathrooms_;
     }
-private:
+
+    virtual ~HotelRoom() {}     // FIX 2: virtual destructor
+
+protected:
     int bedrooms_;
     int bathrooms_;
 };
@@ -21,9 +22,9 @@ private:
 class HotelApartment : public HotelRoom {
 public:
     HotelApartment(int bedrooms, int bathrooms) 
-    : HotelRoom(bedrooms, bathrooms) {}
+        : HotelRoom(bedrooms, bathrooms) {}
 
-    int get_price() {
+    int get_price() override {
         return HotelRoom::get_price() + 100;
     }
 };
@@ -32,11 +33,12 @@ int main() {
     int n;
     cin >> n;
     vector<HotelRoom*> rooms;
+
     for (int i = 0; i < n; ++i) {
         string room_type;
-        int bedrooms;
-        int bathrooms;
+        int bedrooms, bathrooms;
         cin >> room_type >> bedrooms >> bathrooms;
+
         if (room_type == "standard") {
             rooms.push_back(new HotelRoom(bedrooms, bathrooms));
         } else {
@@ -46,13 +48,12 @@ int main() {
 
     int total_profit = 0;
     for (auto room : rooms) {
-        total_profit += room->get_price();
+        total_profit += room->get_price(); // polymorphism works
     }
+
     cout << total_profit << endl;
 
-    for (auto room : rooms) {
-        delete room;
-    }
+    for (auto room : rooms) delete room;
     rooms.clear();
 
     return 0;
